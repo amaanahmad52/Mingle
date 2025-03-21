c3b7f301c1f4db979fc1202060d5629e95300321
Amaan_GIT <amaanahmad52@gmail.com>
2025-03-21 16:53:57 +0530

    login popup

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,51 +1,46 @@
-import Logoicon from '@mui/icons-material/WhatsApp';
-import SearchIcon from '@mui/icons-material/Search';
-import { Avatar } from '@mui/material';
-import { MultiAvatars } from '../assets/utilityComponents/MultiAvatars';
-import DuoIcon from '@mui/icons-material/Duo';
-import CallIcon from '@mui/icons-material/Call';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Logoicon from "@mui/icons-material/WhatsApp";
+import SearchIcon from "@mui/icons-material/Search";
+import { Avatar } from "@mui/material";
+import { MultiAvatars } from "../assets/utilityComponents/MultiAvatars";
+import DuoIcon from "@mui/icons-material/Duo";
+import CallIcon from "@mui/icons-material/Call";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { motion } from "framer-motion";
-import MessageInput from '../assets/utilityComponents/messageBox';
-import MessageContainer from '../assets/utilityComponents/MessageContainer';
-import { useRef, useState ,useEffect, useContext} from 'react';
-import Sidebar from '../assets/utilityComponents/Sidebar';
-import SimpleDialogDemo from '../assets/utilityComponents/DialogBox';
-import ViewListIcon from '@mui/icons-material/ViewList';
-import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
-import axios from 'axios';
-import ProfileTile from '../assets/utilityComponents/ProfileTile';
-import { useSelector } from 'react-redux';
-import Spinner from '../assets/utilityComponents/Spinner';
+import MessageInput from "../assets/utilityComponents/messageBox";
+import MessageContainer from "../assets/utilityComponents/MessageContainer";
+import { useRef, useState, useEffect, useContext } from "react";
+import Sidebar from "../assets/utilityComponents/Sidebar";
+import SimpleDialogDemo from "../assets/utilityComponents/DialogBox";
+import ViewListIcon from "@mui/icons-material/ViewList";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import Spinner from "../assets/utilityComponents/Spinner";
+import { SidebarContext } from "../Context/SideBarContext";
+import MessagesBar from "../assets/utilityComponents/MessagesBar";
+import SettingsBar from "../assets/utilityComponents/SettingsBar";
+import DashBoardBar from "../assets/utilityComponents/DashBoardBar";
+import CallsBar from "../assets/utilityComponents/CallsBar";
+import FilesBar from "../assets/utilityComponents/FilesBar";
+import { Link } from "react-router-dom";
 const URL = import.meta.env.VITE_BACKEND_URL;
 
-import { SidebarContext } from '../Context/SideBarContext';
-import MessagesBar from '../assets/utilityComponents/MessagesBar';
-import SettingsBar from '../assets/utilityComponents/SettingsBar';
-import DashBoardBar from '../assets/utilityComponents/DashBoardBar';
-import CallsBar from '../assets/utilityComponents/CallsBar';
-import FilesBar from '../assets/utilityComponents/FilesBar';
-
-
-
 const Home = () => {
-    const{SideBarselected}=useContext(SidebarContext);
+    const { SideBarselected } = useContext(SidebarContext);
     const switcher = useRef();
     const [sidebarOpen, setSidebarOpen] = useState(true);
     const [searchOpen, setSearchOpen] = useState(false);
     const [searchQuery, setSearchQuery] = useState("");
     const [selectedSearch, setSelectedSearch] = useState("Pankaj Nunnu");
     const { user } = useSelector((state) => state.userReducer);
- 
     const [users, setUsers] = useState([]);
-    
+    const [showLoginPopup, setShowLoginPopup] = useState(false);
 
     useEffect(() => {
         const fetchUsers = async () => {
             try {
                 const response = await axios.get(`${URL}/getalluser`, {
                     withCredentials: true,
-                    headers: { "Content-Type": "application/json" }
+                    headers: { "Content-Type": "application/json" },
                 });
                 console.log("API Response:", response.data); // Debugging
                 setUsers(response.data);
@@ -55,17 +50,23 @@ const Home = () => {
         };
         fetchUsers();
     }, []);
-      
 
-    
-    
-    
+    useEffect(() => {
+        if (!user) {
+            const timer = setTimeout(() => {
+                setShowLoginPopup(true);
+            }, 10000); 
+
+            return () => clearTimeout(timer); // Cleanup on unmount or user login
+        } else {
+            setShowLoginPopup(false);
+        }
+    }, [user]);
+
     const handleSearchChange = (e) => {
         setSearchQuery(e.target.value);
-      
     };
 
-   
     const handleSearchSubmit = (e) => {
         if (e.key === "Enter") {
             setSearchOpen(false);
@@ -76,85 +77,102 @@ const Home = () => {
 
     return (
         <>
-        {!user?(
-            <Spinner/>
-        ):(<>
-            <div className="w-full h-screen overflow-hidden font-mono" style={{ fontFamily: 'Poppins' }}>
-                <div className="flex flex-row w-full h-16">
-                    <div className={`text-center ${sidebarOpen ? "w-1/5 p-4" : "w-1/15 p-5"} flex ${sidebarOpen ? "justify-between" : "flex-col items-start"}`} ref={switcher}>
-                        <Logoicon className="text-cyan-600 mx-3 hover:text-cyan-800 cursor-pointer" />
-                        {sidebarOpen && <span>Mingle</span>}
-                        <ViewListIcon className="text-cyan-600 mx-3 hover:text-cyan-800 cursor-pointer" onClick={clickSidebar} />
-                    </div>
-                <div className="items-center text-center w-1/3 p-4 flex justify-between max-sm:hidden">
-                    {!searchOpen && (
-                        <span className="text-xl font-extrabold flex-grow text-center truncate">
-                            {SideBarselected}
-                        </span>
-                    )}
-                    <div className="relative">
-                        {searchOpen ? (
-                            <input
-                                type="text"
-                                value={searchQuery}
-                                onChange={handleSearchChange}
-                                onKeyDown={handleSearchSubmit}
-                                className="border border-gray-400 rounded px-2 py-1 focus:outline-none"
-                                placeholder="Search..."
-                            />
-                        ) : (
-                            <SearchIcon className="hover:text-cyan-600 cursor-pointer ml-4" onClick={() => setSearchOpen(true)} />
-                        )}
-                    </div>
-                    <MultiAvatars count={`+${user?.friends?.length}`} />
-                </div>
-
-                
-    
-                    <div className="rounded-1xl text-center w-2/3 p-4 flex justify-between border-1 border-solid max-sm:h-full max-sm:w-full">
-                        <Avatar className="text-cyan-600 cursor-pointer" />
-                        <span>{selectedSearch}</span>
-                        <div className='flex'>
-                            <DuoIcon className="mt-1 hover:text-cyan-700 mx-1 cursor-pointer" />
-                            <CallIcon className="mt-1 hover:text-cyan-700 mx-1 cursor-pointer" />
-                            <SimpleDialogDemo />
-                        </div>
-                    </div>
-                </div>
-                <div className="flex w-full h-[calc(100vh-4rem)]">
-                    <Sidebar st={sidebarOpen} />
+            {!user ? (
+                <>
                     
-                    <div className="w-1/3 p-4 flex flex-wrap gap-2">
-
-                        {/* middle box */}
-                        <div className='py-2 gap-2 w-full flex flex-col overflow-auto'>
-
-                            {(SideBarselected==="Messages") && <MessagesBar setSelectedSearch={setSelectedSearch} searchQuery={searchQuery} users={users}/>}
-                            {(SideBarselected==="Settings") && <SettingsBar />}
-                            {(SideBarselected==="Dashboard") && <DashBoardBar friend={users.user} />}
-                            {(SideBarselected==="Calls") && <CallsBar />}
-                            {(SideBarselected==="Files") && <FilesBar />}
-                            
-                        </div>
-                    
-                    </div>
-
-                    <div className="w-2/3 p-4 flex flex-col justify-between max-sm:h-full max-sm:w-full">
-                        <motion.div className="flex flex-col gap-2 overflow-y-scroll scrollbar-hidden" animate={{ y: [0, -100, 0] }} transition={{ ease: "easeInOut", duration: 0.5 }}>
-                            <MessageContainer message={"hi what are u doing"} fromMe={true} />
-                            <MessageContainer message={"nothing"} fromMe={false} />
-                            <MessageContainer message={"ohk"} fromMe={true} />
-                            <MessageContainer message={"i am now at home by the way"} fromMe={false} />
+                    {!showLoginPopup?(<Spinner />) : (
+                        <motion.div
+                            className="fixed top-1/3 left-1/2 transform -translate-x-1/2 bg-[#f43098] text-white p-4 rounded-lg shadow-lg"
+                            initial={{ opacity: 0, scale: 0.8 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            exit={{ opacity: 0, scale: 0.8 }}
+                            transition={{ duration: 0.5, ease: "easeInOut" }}
+                        >
+                            Please <Link to={"/login"} className="underline hover:text-[#301b25]">login</Link>. You are not logged in.
                         </motion.div>
-                        <MessageInput />
+                    )}
+                </>
+            ) : (
+                <>
+                    <div className="w-full h-screen overflow-hidden font-mono" style={{ fontFamily: "Poppins" }}>
+                        <div className="flex flex-row w-full h-16">
+                            <div
+                                className={`text-center ${
+                                    sidebarOpen ? "w-1/5 p-4" : "w-1/15 p-5"
+                                } flex ${sidebarOpen ? "justify-between" : "flex-col items-start"}`}
+                                ref={switcher}
+                            >
+                                <Logoicon className="text-cyan-600 mx-3 hover:text-cyan-800 cursor-pointer" />
+                                {sidebarOpen && <span>Mingle</span>}
+                                <ViewListIcon className="text-cyan-600 mx-3 hover:text-cyan-800 cursor-pointer" onClick={clickSidebar} />
+                            </div>
+
+                            <div className="items-center text-center w-1/3 p-4 flex justify-between max-sm:hidden">
+                                {!searchOpen && (
+                                    <span className="text-xl font-extrabold flex-grow text-center truncate">
+                                        {SideBarselected}
+                                    </span>
+                                )}
+                                <div className="relative">
+                                    {searchOpen ? (
+                                        <input
+                                            type="text"
+                                            value={searchQuery}
+                                            onChange={handleSearchChange}
+                                            onKeyDown={handleSearchSubmit}
+                                            className="border border-gray-400 rounded px-2 py-1 focus:outline-none"
+                                            placeholder="Search..."
+                                        />
+                                    ) : (
+                                        <SearchIcon className="hover:text-cyan-600 cursor-pointer ml-4" onClick={() => setSearchOpen(true)} />
+                                    )}
+                                </div>
+                                <MultiAvatars count={`+${user?.friends?.length}`} />
+                            </div>
+
+                            <div className="rounded-1xl text-center w-2/3 p-4 flex justify-between border-1 border-solid max-sm:h-full max-sm:w-full">
+                                <Avatar className="text-cyan-600 cursor-pointer" />
+                                <span>{selectedSearch}</span>
+                                <div className="flex">
+                                    <DuoIcon className="mt-1 hover:text-cyan-700 mx-1 cursor-pointer" />
+                                    <CallIcon className="mt-1 hover:text-cyan-700 mx-1 cursor-pointer" />
+                                    <SimpleDialogDemo />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex w-full h-[calc(100vh-4rem)]">
+                            <Sidebar st={sidebarOpen} />
+
+                            <div className="w-1/3 p-4 flex flex-wrap gap-2">
+                                <div className="py-2 gap-2 w-full flex flex-col overflow-auto">
+                                    {SideBarselected === "Messages" && (
+                                        <MessagesBar setSelectedSearch={setSelectedSearch} searchQuery={searchQuery} users={users} />
+                                    )}
+                                    {SideBarselected === "Settings" && <SettingsBar />}
+                                    {SideBarselected === "Dashboard" && <DashBoardBar friend={users.user} />}
+                                    {SideBarselected === "Calls" && <CallsBar />}
+                                    {SideBarselected === "Files" && <FilesBar />}
+                                </div>
+                            </div>
+
+                            <div className="w-2/3 p-4 flex flex-col justify-between max-sm:h-full max-sm:w-full">
+                                <motion.div
+                                    className="flex flex-col gap-2 overflow-y-scroll scrollbar-hidden"
+                                    animate={{ y: [0, -100, 0] }}
+                                    transition={{ ease: "easeInOut", duration: 0.5 }}
+                                >
+                                    <MessageContainer message={"hi what are u doing"} fromMe={true} />
+                                    <MessageContainer message={"nothing"} fromMe={false} />
+                                </motion.div>
+                                <MessageInput />
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            </>)}
+                </>
+            )}
         </>
     );
 };
 
 export default Home;
-
-
